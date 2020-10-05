8bffdd1a05d58687788c88169df5d630f7493ee7
Samus115 <guillaumme.gay.74@gmail.com>
2020-10-05 14:31:31 +0200

    ajout des fonction ajout commune, saisie entier et saisie Nom

---
+++ MyApp/MyApp/Commune.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp
+{
+    class Commune
+    {
+        public string Nom;
+        public int CodePost;
+        public int NbHab;
+    }
+}
--- MyApp/MyApp/Program.cs
+++ MyApp/MyApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyApp
 {
@@ -6,7 +7,30 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            string choix = Menu();
+            List<Commune> listcommune = new List<Commune>();
+
+            while(true)
+            {
+                string choix = Menu();
+
+                if(choix == "1")
+                {
+                   Commune c = ajouterCommune();
+                    listcommune.Add(c);
+                }
+                else if(choix == "2")
+                {
+
+                }
+                else if(choix == "q")
+                {
+
+                }
+                else
+                {
+                    break;
+                }
+            }
 
         }
 
@@ -15,9 +39,40 @@ namespace MyApp
             Console.WriteLine("Bienvenue dans l'application de gestion de communes");
             Console.WriteLine("Que voulez-vous faire");
             Console.WriteLine("1.Créer une nouvelle communes");
-            Console.WriteLine("2. Afficher l'ensemble des communes");
+            Console.WriteLine("2.Afficher l'ensemble des communes");
+            Console.WriteLine("q.Quitter");
             string choix = Console.ReadLine();
             return choix;
+        }
+
+        public static Commune ajouterCommune()
+        {
+            Commune c = new Commune();
+            c.Nom = saisieNom("Quel est le nom de votre ville ?");
+            c.CodePost = saisieEntier("Quel est de code postal ?");
+            c.NbHab = saisieEntier("Combie y a-t-il d'habitants ?");
+
+            return c;
+        }
+
+        public static int saisieEntier(string message)
+        {
+            int valeurconvertie;
+            Console.WriteLine(message);
+            string entier = Console.ReadLine();
+
+            while(!int.TryParse(entier, out valeurconvertie))
+            {
+                Console.WriteLine("Veuillez saisir un entier correcte");
+                entier = Console.ReadLine();
+            }
+            return valeurconvertie;
+        }
+        public static string saisieNom(string message)
+        {
+            Console.WriteLine(message);
+            string Nom = Console.ReadLine();
+            return Nom;
         }
     }
 }
